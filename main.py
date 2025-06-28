ad27434dc16f7a4a221e78f3607b8e5c64126685
edbrizuela <brizuela.edgar@outlook.com>
2025-06-28 15:20:06 -0600

    Add CORS middleware to allow connections from React frontend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware # Importing CORS middleware
 from services.github_service import get_user_data
+
 
 app = FastAPI()
 
@@ -9,3 +11,13 @@ async def github_user_info(username: str):
     if not data:
         raise HTTPException(status_code=404, detail="User not found")
     return data
+
+
+# Let connection from React app or frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React port 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
